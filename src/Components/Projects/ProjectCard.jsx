ba49dde026a7cf9c353473c25fa8b8a61edcd8ea
Hasham Ahmed <ahmedhasham90012@gmail.com>
2024-04-10 21:57:49 +0500

    responsiveness fixed

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -11,7 +11,7 @@ import {react,js,html,css,tailwind,framer,firebase} from '../../assets/index'
  
 export function ProjectCard({img,title,description,className,link}) {
   return (
-    <div className={` flex flex-col md:flex-row justify-center items-center md:justify-between p-16 mt-32 ${className}`}>
+    <div className={` flex flex-col md:flex-row justify-center items-center md:justify-between md:p-16 mt-32 ${className}`}>
 
     <Card className="w-72 flex justify-center bg-black items-center text-white h-96 ring-2 ring-violet-600 shadow-lg shadow-violet-500">
       <CardHeader floated={false} className="w-40 bg-black  ">
@@ -30,27 +30,27 @@ className="hover:text-blue-500  font-bold cursor-pointer flex justify-center ite
     </Card>
     <div className="tech mt-14 md:mt-0">
       <h2 className="text-[4vw] text-white text-center font-semibold carter-one-regular">Tech Used</h2>
-      <div className="images flex flex-wrap max-w-[500px] justify-center  gap-4 mt-10">
-        <div className="w-24 h-24 rounded-full ring-2 p-2 flex justify-center items-center">
+      <div className="images flex flex-wrap max-w-[500px] justify-center  gap-2 mt-10">
+        <div className="w-20 h-20 rounded-full ring-2 p-2 flex justify-center items-center">
           <img className="w-18 object-contain object-center" src={react} alt="react" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-2 ring-red-500  flex justify-center items-center">
+        <div className="w-20 h-20 rounded-full ring-2 ring-red-500  flex justify-center items-center">
           <img  className="w-18 object-center"src={html} alt="html" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-blue-600 ring-2 p-2 flex justify-center items-center">
-          <img className="w-14 object-contain object-center" src={css} alt="css" />
+        <div className="w-20 h-20 rounded-full ring-blue-600 ring-2 p-2 flex justify-center items-center">
+          <img className="w-12 object-contain object-center" src={css} alt="css" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-blue-500  ring-2 p-2 flex justify-center items-center">
+        <div className="w-20 h-20 rounded-full ring-blue-500  ring-2 p-2 flex justify-center items-center">
           <img className="w-16 object-contain object-center" src={tailwind} alt="tailwind" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-2 ring-yellow-600  p-2 flex justify-center items-center">
+        <div className="w-20 h-20 rounded-full ring-2 ring-yellow-600  p-2 flex justify-center items-center">
           <img className="w-18 object-contain object-center" src={js} alt="javascript" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-2 ring-violet-600  p-2 flex justify-center items-center">
+        <div className="w-20 h-20 rounded-full ring-2 ring-violet-600  p-2 flex justify-center items-center">
           <img className="w-18 object-contain object-center" src={framer} alt="framerMotion" />
         </div>
-        <div className="w-24 h-24 rounded-full ring-2 ring-orange-600  p-2 flex justify-center items-center">
-          <img className="w-16 object-contain object-center" src={firebase} alt="framerMotion" />
+        <div className="w-20 h-20 rounded-full ring-2 ring-orange-600  p-2 flex justify-center items-center">
+          <img className="w-14 object-contain object-center" src={firebase} alt="framerMotion" />
         </div>
        
       </div>
